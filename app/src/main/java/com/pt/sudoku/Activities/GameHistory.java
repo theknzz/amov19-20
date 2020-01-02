ffb59ac73d521c30d87c1682eeeab205a4f9791e
Unknown <andre.pessoa.coelho@gmail.com>
2020-01-02 00:34:16 +0000

    Game History complete

--- app/src/main/java/com/pt/sudoku/Activities/GameHistory.java
+++ app/src/main/java/com/pt/sudoku/Activities/GameHistory.java
@@ -4,12 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.LauncherActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.JsonReader;
 import android.widget.ListView;
 
 import com.pt.sudoku.History.CustomAdapter;
 import com.pt.sudoku.History.HistoryModel;
 import com.pt.sudoku.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class GameHistory extends AppCompatActivity {
@@ -25,36 +36,53 @@ public class GameHistory extends AppCompatActivity {
 
         dataModels = new ArrayList<>();
 
-        dataModels.add(new HistoryModel("1", "Easy", "12:45",""));
-        dataModels.add(new HistoryModel("3", "Hard", "13","Me"));
-        dataModels.add(new HistoryModel("2", "Easy", "10","Me"));
-        dataModels.add(new HistoryModel("3", "Easy", "2","Not me"));
-        dataModels.add(new HistoryModel("2", "Hard", "23","Me"));
-        dataModels.add(new HistoryModel("1", "Easy", "5:45",""));
-        dataModels.add(new HistoryModel("1", "Hard", "12:45",""));
-        dataModels.add(new HistoryModel("1", "Easy", "12:45",""));
-        dataModels.add(new HistoryModel("3", "Hard", "12:45","Not me"));
-        dataModels.add(new HistoryModel("1", "Easy", "12:45",""));
-        dataModels.add(new HistoryModel("2", "Easy", "12:45","Not me"));
-        dataModels.add(new HistoryModel("3", "Hard", "12:45","Not me"));
-        dataModels.add(new HistoryModel("1", "Easy", "12:45",""));
-        dataModels.add(new HistoryModel("1", "Hard", "12:45",""));
-        dataModels.add(new HistoryModel("1", "Easy", "12:45",""));
-        dataModels.add(new HistoryModel("3", "Hard", "12:45","Not me"));
-        dataModels.add(new HistoryModel("1", "Easy", "12:45",""));
-        dataModels.add(new HistoryModel("2", "Easy", "12:45","Not me"));
-        dataModels.add(new HistoryModel("3", "Hard", "12:45","Not me"));
-        dataModels.add(new HistoryModel("1", "Easy", "12:45",""));
-        dataModels.add(new HistoryModel("1", "Hard", "12:45",""));
-        dataModels.add(new HistoryModel("1", "Easy", "12:45",""));
-        dataModels.add(new HistoryModel("3", "Hard", "12:45","Not me"));
-        dataModels.add(new HistoryModel("1", "Easy", "12:45",""));
-        dataModels.add(new HistoryModel("2", "Easy", "12:45","Not me"));
-        dataModels.add(new HistoryModel("3", "Hard", "12:45","Not me"));
-        dataModels.add(new HistoryModel("1", "Easy", "12:45",""));
+        loadHistory(dataModels);
 
         adapter= new CustomAdapter(dataModels,getApplicationContext());
 
         listView.setAdapter(adapter);
+    }
+
+    private void loadHistory(ArrayList<HistoryModel> dataModels) {
+        File file = new File(Environment.getExternalStorageDirectory(), "history_file.json");
+        if(!file.exists())
+            return;
+
+        String strFileJson;
+        FileInputStream fin = null;
+
+        JSONObject jsonObj = null;
+
+        try {
+            fin = new FileInputStream(file);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fin));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            strFileJson = sb.toString();
+            fin.close();
+
+            jsonObj = new JSONObject(strFileJson);
+            int size = 0;
+            try {
+                size = jsonObj.getInt("size");
+            }catch (JSONException e){
+            }
+            JSONObject game = null;
+            for(int i = 1; i <= size; i++){
+                game = (JSONObject) jsonObj.get("game"+i);
+                dataModels.add(new HistoryModel(
+                                game.getString("mode"),
+                                game.getString("difficulty"),
+                                game.getString("result"),
+                                game.getString("winner")));
+            }
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
--- app/src/main/java/com/pt/sudoku/Activities/MainActivity.java
+++ app/src/main/java/com/pt/sudoku/Activities/MainActivity.java
@@ -1,8 +1,14 @@
 package com.pt.sudoku.Activities;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -11,10 +17,21 @@ import com.pt.sudoku.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
+
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            }, 152);
+            return;
+        }
+
     }
 
     public void onPlay(View view) {
